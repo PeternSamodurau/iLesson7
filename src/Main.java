5368e33a020c38f4e96d913ef5176c4d14110cd9
Peter Samadurau <spvrent@mail.ru>
2022-12-02 10:48:59 +0300

    title chenge 3

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
+import java.time.LocalDateTime;
+
 public class Main {
     public static void main(String[] args) {
+        LocalDateTime localDateTime = LocalDateTime.now();
+        System.out.println(localDateTime);
       for (int x = 1; x <=13; x++){
 
           switch (x){
               case 1: case 2: case 9:
-                  System.out.println(x + "зима");
+                  System.out.println(x + "зима  ");
                   break;
               default:
                   System.out.println("нет такого месяца");
